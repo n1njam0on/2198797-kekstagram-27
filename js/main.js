af2acc5adc00b27bd3276f1a8a2c546cb77b4018
Vitalii Beliaev <vtaliy.belyaev@gmail.com>
2022-10-01 18:52:48 +0200

    Добавил две вспомогательные функции

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function getRundomNumber(min, max){
+    if(min < 0 || max < 0 || min > max){
+        return NaN
+    }
+    return Math.random() * (max - min) + Number(min);
+}
+
+
+function lengthCompare(str, maxLength){
+    if(typeof maxLength !== 'number'){
+        return NaN;
+    }
+    if(String(str).length <= maxLength){
+        return true;
+    }
+    return false;
+}
