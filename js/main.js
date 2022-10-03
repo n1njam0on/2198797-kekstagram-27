5df9113cd9b33bfc3056aac1d1914a31f6a0450e
Vitalii Beliaev <vtaliy.belyaev@gmail.com>
2022-10-03 09:27:04 +0200

    Исправил ошибки линтера

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
-function getRundomNumber(min, max){
-    if(min < 0 || max < 0 || min > max){
-        return NaN;
-    }
-    return Math.random() * (max - min) + Number(min);
+function getRandomNumber(min, max){
+  if(min < 0 || max < 0 || min > max){
+    return NaN;
+  }
+  return Math.random() * (max - min) + Number(min);
 }
 
 
-function lengthCompare(str, maxLength){
-    if(typeof maxLength !== 'number'){
-        return NaN;
-    }
-    if(String(str).length <= maxLength){
-        return true;
-    }
-    return false;
+function getLengthCompare(str, maxLength){
+  if(typeof maxLength !== 'number'){
+    return NaN;
+  }
+  if(String(str).length <= maxLength){
+    return true;
+  }
+  return false;
 }
+
+getRandomNumber(1,10);
+getLengthCompare('Hello world!', 5);
