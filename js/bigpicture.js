a9bc3f1d6968124997ce99a5ed7d20c37c141cdb
Vitalii Beliaev <vtaliy.belyaev@gmail.com>
2022-10-30 17:56:43 +0800

    Добавил загрузку комментариев к изображению

--- js/bigpicture.js
+++ js/bigpicture.js
@@ -3,6 +3,44 @@ import { isEscapeKey } from './util.js';
 
 const bigPicture = document.querySelector('.big-picture');
 const closeButton = bigPicture.querySelector('.big-picture__cancel');
+const uploadButton = bigPicture.querySelector('.comments-loader');
+const commentsBlock = bigPicture.querySelector('.social__comments');
+
+let commentsNumber = 0;
+let commentsCounter = 0;
+let tmpCommentsData = [];
+
+const NUMBER_UPLOADED_COMMENTS = 5;
+
+
+const uploadComments = () => {
+  const picturesFragment = document.createDocumentFragment();
+  for (let i = 0; i < NUMBER_UPLOADED_COMMENTS; i++){
+    if (tmpCommentsData.length <= 0){
+      break;
+    }
+    const data = tmpCommentsData.pop();
+
+    const newLi = document.createElement('li');
+    newLi.classList.add('social__comment');
+
+    const avatar = document.createElement('img');
+    avatar.classList.add('social__picture');
+    avatar.src = data.avatar;
+    avatar.alt = data.name;
+    newLi.appendChild(avatar);
+
+    const message = document.createElement('p');
+    message.classList.add('social__text');
+    message.textContent = data.message;
+    newLi.appendChild(message);
+
+    picturesFragment.appendChild(newLi);
+    commentsCounter++;
+  }
+  bigPicture.querySelector('.social__comment-count').innerHTML = `${commentsCounter} из <span class="comments-count">${commentsNumber}</span> комментариев`;
+  commentsBlock.appendChild(picturesFragment);
+};
 
 const onBigPictureEscKeydown = (evt) => {
   if(isEscapeKey(evt)){
@@ -10,49 +48,34 @@ const onBigPictureEscKeydown = (evt) => {
     bigPicture.classList.add('hidden');
     document.querySelector('body').classList.remove('modal-open');
     document.removeEventListener('keydown', onBigPictureEscKeydown);
+    uploadButton.removeEventListener('click', uploadComments);
   }
 };
 
 const onBigPictureCloseButton = () => {
   bigPicture.classList.add('hidden');
   document.querySelector('body').classList.remove('modal-open');
+  document.removeEventListener('keydown', onBigPictureEscKeydown);
   closeButton.removeEventListener('click', onBigPictureCloseButton);
+  uploadButton.removeEventListener('click', uploadComments);
 };
+
 
 export const renderBigPicture = (id) => {
   bigPicture.classList.remove('hidden');
   const photoData = similarCards.find((element) => element.id === id);
+  tmpCommentsData = photoData.comments.slice(0);
+  commentsCounter = 0;
 
   bigPicture.querySelector('.big-picture__img').querySelector('img').src = photoData.url;
   bigPicture.querySelector('.likes-count').textContent = photoData.likes;
-  bigPicture.querySelector('.comments-count').textContent = photoData.comments.length;
+  commentsNumber = photoData.comments.length;
   bigPicture.querySelector('.social__caption').textContent = photoData.description;
 
-  const commentsBlock = bigPicture.querySelector('.social__comments');
   commentsBlock.innerHTML = '';
-  const picturesFragment = document.createDocumentFragment();
+  uploadComments();
+  uploadButton.addEventListener('click', uploadComments);
 
-  photoData.comments.forEach((element) => {
-    const newLi = document.createElement('li');
-    newLi.classList.add('social__comment');
-
-    const avatar = document.createElement('img');
-    avatar.classList.add('social__picture');
-    avatar.src = element.avatar;
-    avatar.alt = element.name;
-    newLi.appendChild(avatar);
-
-    const message = document.createElement('p');
-    message.classList.add('social__text');
-    message.textContent = element.message;
-    newLi.appendChild(message);
-
-    picturesFragment.appendChild(newLi);
-  });
-  commentsBlock.appendChild(picturesFragment);
-
-  bigPicture.querySelector('.social__comment-count').classList.add('hidden');
-  bigPicture.querySelector('.comments-loader').classList.add('hidden');
   document.querySelector('body').classList.add('modal-open');
 
   closeButton.addEventListener('click', onBigPictureCloseButton);
